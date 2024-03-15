5c27a047ddaa31dcc2e6c4bd8289ae4239bc76f4
edilson santiago <edilsonsantiagoe7@gmail.com>
2024-03-15 10:49:14 -0300

    findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -14,7 +16,13 @@ public class Program {
 
 		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
 		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
+		Seller seller1 = sellerDao.findById(3);
+		
 		System.out.println(seller);
+		
+		System.out.println(seller1);
 	}
 
 }
